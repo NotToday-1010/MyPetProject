eb6a26c9799c666f308d756d8425524b99431ddd
Iskander <iskandersdkv@gmail.com>
2023-02-19 22:27:16 +0300

    add Loader, not existing page. change sidebar toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -18,9 +18,18 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={classNames(
+                cls.Sidebar,
+                { [cls.collapsed]: collapsed },
+                [className],
+            )}
         >
-            <Button onClick={onToggle}>toggle</Button>
+            <Button
+                className={cls.nav_open_button}
+                onClick={onToggle}
+            >
+                {collapsed ? <>Open</> : <>Close</>}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
